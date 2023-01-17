c287243653972d9b0261467e29b206f32a2c69e9
Ian Shi <72175372+lifeablestuff@users.noreply.github.com>
2023-01-17 15:05:52 -0800

    trying to integrate TCP connection
    variable typos and such still have to be fixed

--- battleship.py
+++ battleship.py
@@ -16,38 +16,57 @@ class game(Fl_Window):
 		self.ships = []
 		for row in range(5):
 			for col in range(5):
-				self.clientbl.append(Fl_Button(col*50+20,row*50+80,50,50))
+				self.clientbl.append(Fl_Button(col*50+20,row*50+140,50,50))
 				self.clientbl[-1].image(self.seaimg)
+				self.clientbl[-1].callback(self.comms_test)
 				#self.bl[-1].callback(self.button_click)
 				#num += 1
 				self.cords.append([row,col])
 		
 		for row in range(5):
 			for col in range(5):
-				self.oppbl.append(Fl_Button(col*50+400,row*50+80,50,50))
+				self.oppbl.append(Fl_Button(col*50+400,row*50+140,50,50))
 				self.oppbl[-1].image(self.seaimg)
 				#self.oppbl[-1].callback(self.button_click)
 				#num += 1
 				#self.cords.append([row,col])
-		self.confirm = Fl_Button(20,350,80,40,'ready')
-		self.you = Fl_Box(100,20,80,40,'You')
-		self.enemy = Fl_Box(500,20,80,40,'Enemy')
-		self.confirm.hide()
+		self.confirm = Fl_Button(110,500,80,40,'ready')
+		self.you = Fl_Box(100,110,80,40,'You')
+		self.enemy = Fl_Box(500,110,80,40,'Enemy')
+		#self.confirm.hide()
+		self.game_message = Fl_Box(300,20,80,40)
+		self.game_message.label('Game message')
+		#self.game_message.hide()
 		self.redraw()
+		self.allow_conn = Fl_Button(260,80,150,40,'Allow connection')
 		self.end()
 		self.sock = None
-		'''
-		self.host = sys.argv[2]
-		self.port=int(sys.argv[3])
-		self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-		if sys.argv[1] == "server":
-			self.sock.bind((self.host, self.port)) # servers bind
+		self.game_state = 'Inactive'
 		
+		conn_to_client = None
 		
-		fd=self.sock.fileno()
-		Fl.add_fd( fd, self.receive_data)
-		self.confirm_conn()
-		'''
+		host = 'localhost'
+		port = 5555
+		self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #fd=3
+		if sys.argv[1] == 'server':
+			self.sock.bind((host, port))
+			self.sock.listen()
+			fdl=self.sock.fileno() #listening fd
+			Fl.add_fd(fdl, self.acceptConnection) 
+		
+		elif sys.argv[1] == 'client':
+			self.sock.connect((host,port))
+			self.fd=self.sock.fileno()
+			Fl.add_fd(self.fd, self.receive_data)
+			
+		
+	def acceptConnection(self, fdl): #runs when data comes to socket s
+		conn, raddr = self.sock.accept()
+		fd=conn.fileno() #file descriptor for new established connection
+		Fl.add_fd(fd, self.recv_data)
+		self.other_person=conn #add fd(key) and conn(value) to connD dict
+		
+
 	def confirm_conn(self):
 		if sys.argv[2] == 'server':
 			return None
@@ -56,23 +75,28 @@ class game(Fl_Window):
 			self.sock.sendto('connect',(self.host,self.port))
 		
 	def recv_data(self,fd):
-		message = []
-		(text, self.addr)=self.s.recvfrom(1024)
-		message = (text.decode())
-		
-		if message == 'connect':
-			self.sock.sendto('confirmed',(self.addr))
-			fl_message('a player has connected')
-			fl_message('planning phase has started')
-			
-		elif message == 'confirmed':
-			fl_message('connection confirmed')
-			fl_message('planning phase has started')
-			
-	def planning_phase(self,wid):
-		return None
+		data= self.conn_to_client.recv(1024)
+		print(data)
+
+	def receive_data(self,fd):
+		data=self.sock.recv(1024)
+		print(data)
+
+	def planning_phase(self):
+		self.game_message.show()
+		self.isplanning = 'Planning'
 	
-		
+	def but_callback(self,wid):
+		if self.isplanning == 'Planning': #checking if still planning
+			if len(self.boats) < 4:
+				if wid in self.oppbl:
+					return None
+				self.boats.append(self.cords[self.client_but.index(wid)])
+	
+	def comms_test(self,wid):
+		self.sock.sendall((self.cords[self.clientbl.index(wid)]).encode())
+
+
 	
 	
 battleship = game(800,800,'game')
